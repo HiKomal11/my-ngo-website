9d30dcf59cf6da48f720833e5a912ff130acf021
unknown <krpandey1106@gmail.com>
2025-12-29 16:39:52 +0530

    Unify router setup with HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,36 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+
+// Public Pages
 import HomePage from "./pages/Home";
-import About from "./pages/AboutUs";
-import Work from "./pages/OurWork";
+import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
 import Projects from "./pages/Projects";
+import Media from "./pages/Media";
 import GetInvolved from "./pages/GetInvolved";
-import Contact from "./pages/ContactUs";
-import DonatePage from "./pages/DonatePage";
-import DonationsList from "./components/DonationsList";
 import BlogPage from "./pages/BlogPage";
 import BlogDetail from "./pages/BlogDetail";
-import VolunteerPage from "./components/VolunteerPage";
+import ContactUs from "./pages/ContactUs";
+import DonatePage from "./pages/DonatePage";
+
+// Admin / Forms
+import Dashboard from "./pages/Dashboard";
 import VolunteerForm from "./pages/VolunteerForm";
 import PartnerForm from "./pages/PartnerForm";
 import FundraiseForm from "./pages/FundraiseForm";
-import CampaignsPage from "./pages/CampaignsPage";
-import CommunitySubscribe from "./pages/CommunitySubscribe";
 import SubscriptionList from "./pages/SubscriptionList";
-import Dashboard from "./pages/Dashboard";
-import CampaignJoinForm from "./pages/CampaignJoinForm";
+import CampaignList from "./pages/CampaignList";
 import CampaignParticipationList from "./pages/CampaignParticipationList";
 import CampaignDetail from "./pages/CampaignDetail";
 import CampaignCreateForm from "./pages/CampaignCreateForm";
-import CampaignList from "./pages/CampaignList";
-import Media from "./pages/Media";
+import CampaignJoinForm from "./pages/CampaignJoinForm";
+import CampaignsPage from "./pages/CampaignsPage";
+import CommunitySubscribe from "./pages/CommunitySubscribe";
+
+// Extra Components
+import DonationsList from "./components/DonationsList";
+import VolunteerPage from "./components/VolunteerPage";
 import MediaUploadForm from "./components/MediaUploadForm";
 import MediaList from "./components/MediaList";
 
@@ -48,35 +54,39 @@ export default function App() {
         {/* Page Content */}
         <main className="flex-grow-1">
           <Routes>
+            {/* Public Routes */}
             <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/work" element={<Work />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/work" element={<OurWork />} />
             <Route path="/projects" element={<Projects />} />
+            <Route path="/media" element={<Media />} />
             <Route path="/get-involved" element={<GetInvolved />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/:id" element={<BlogDetail />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/donate" element={<DonatePage />} />
+
+            {/* Admin / Dashboard Routes */}
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/volunteer" element={<VolunteerPage />} />
             <Route path="/volunteer/form" element={<VolunteerForm />} />
             <Route path="/partner" element={<PartnerForm />} />
             <Route path="/fundraise" element={<FundraiseForm />} />
             <Route path="/campaigns" element={<CampaignsPage />} />
-            <Route path="/campaigns/:id/join" element={<CampaignJoinForm />} />
-            <Route path="/campaign-participants" element={<CampaignParticipationList />} />
-            <Route path="/campaigns/:id" element={<CampaignDetail />} />
             <Route path="/campaigns/create" element={<CampaignCreateForm />} />
             <Route path="/campaigns/list" element={<CampaignList />} />
+            <Route path="/campaigns/:id" element={<CampaignDetail />} />
+            <Route path="/campaigns/:id/join" element={<CampaignJoinForm />} />
+            <Route path="/campaign-participants" element={<CampaignParticipationList />} />
             <Route path="/subscribe" element={<CommunitySubscribe />} />
             <Route path="/subscribers" element={<SubscriptionList />} />
-            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/donations" element={<DonationsList />} />
-            <Route path="/donate" element={<DonatePage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:id" element={<BlogDetail />} />
-            <Route path="/media" element={<Media />} />
             <Route path="/media/upload" element={<MediaUploadForm />} />
             <Route path="/media/list" element={<MediaList />} />
-            <Route path="/contact" element={<Contact />} />
+
+            {/* Fallback */}
             <Route path="*" element={<HomePage />} />
           </Routes>
-
         </main>
 
         {/* Footer */}
